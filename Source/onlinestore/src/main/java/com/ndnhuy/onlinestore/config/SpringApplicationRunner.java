d52df2a25b647f628c9a66e3b91aca225bf59671
Sun <ndnhuy2504@gmail.com>
2015-11-10 20:29:45 +0700

    start

---
+++ Source/onlinestore/src/main/java/com/ndnhuy/onlinestore/config/SpringApplicationRunner.java
@@ -0,0 +1,25 @@
+package com.ndnhuy.onlinestore.config;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@ComponentScan("com.ndnhuy.onlinestore")
+@EnableAutoConfiguration
+public class SpringApplicationRunner {
+	 public static void main(String[] args) {
+	        ApplicationContext ctx = SpringApplication.run(SpringApplicationRunner.class, args);
+
+	        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+	        String[] beanNames = ctx.getBeanDefinitionNames();
+	        Arrays.sort(beanNames);
+	        for (String beanName : beanNames) {
+	            System.out.println(beanName);
+	        }
+	    }
+}
---
+++ Source/onlinestore/src/main/java/com/ndnhuy/onlinestore/service/HomeController.java
@@ -0,0 +1,15 @@
+package com.ndnhuy.onlinestore.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String home() {
+		
+		return "WELCOME";
+	}
+}
